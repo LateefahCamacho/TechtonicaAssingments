954056caa5f853c028ba627bca3c047f09722e81
Lateefah camacho <lateefah.camacho07@gmail.com>
2023-01-26 13:17:39 -0800

    create button with dom

---
+++ recipe/script.js
@@ -0,0 +1,10 @@
+const button = document.getElementsByClassName("noteButton")
+  
+
+function notes(){
+//const textArea = document.createElement("text")
+//const myTextArea = document.createTextNode("enter notes here")
+    //textArea.appendChild(myTextArea) 
+    //document.body.appendChild(textArea)
+    //console.log(textArea)
+}
