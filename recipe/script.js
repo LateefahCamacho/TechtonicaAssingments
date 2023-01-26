4bfc201130764f12ff5b754902138dfb98bc024e
Lateefah camacho <lateefah.camacho07@gmail.com>
2023-01-26 13:23:20 -0800

    appendChild dom element

--- recipe/script.js
+++ recipe/script.js
@@ -3,8 +3,8 @@ const button = document.getElementsByClassName("noteButton")
 
 function notes(){
 const textArea = document.createElement("text")
-//const myTextArea = document.createTextNode("enter notes here")
-    //textArea.appendChild(myTextArea) 
+const myTextArea = document.createTextNode("enter notes here")
+    textArea.appendChild(myTextArea) 
     //document.body.appendChild(textArea)
     //console.log(textArea)
 }
