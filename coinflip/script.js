b5ee1c6b3340f399d0f46b9f2e9d5c2da96a08d3
Lateefah camacho <lateefah.camacho07@gmail.com>
2023-02-08 14:10:39 -0800

    created html

---
+++ coinflip/script.js
@@ -0,0 +1,16 @@
+function flipCoin(){
+    //declare variable flip; assign random number: 0-1
+    //0 = heaads; 1 = tails;
+    //llog flip to console
+    //if/else statement
+}
+
+function drawheads() {
+
+}
+
+
+
+function drawTails() {
+    
+}
